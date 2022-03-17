03dc6c26cc4e44117622ca264747b6f7572619f3
gustavomontoli <gustavomont97@gmail.com>
2022-03-17 16:27:55 -0300

    primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Donuts Restaurant in Brasília</title>
+        <meta name="description" content="A piece of heaven in these donuts" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      homepage
+    </div>
+  );
+}
